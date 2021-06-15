10240e69392ece7829d3ab47c00eeac3a46ec4ee
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-16 05:49:50 +0700

    Tugas 3 fix

---
+++ tugas3.js
@@ -0,0 +1,19 @@
+const printSegitiga = 5
+let str=""
+let tipeData = typeof printSegitiga
+
+if(tipeData==='number')
+{
+for(let i=printSegitiga-1;i>=0;i--)
+{
+  
+for(let j=1; j <=i+1; j++){
+    str+=j
+}
+str+="\n"
+}
+console.log(str)
+}
+else if(tipeData==='string'){
+    console.log("Data harus Number")
+}
