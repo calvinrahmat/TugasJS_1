d3a8f22302e1eb21a405033c5c954ea7cafc5b16
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-16 20:48:34 +0700

    Merapihkan line

--- tugas4.js
+++ tugas4.js
@@ -1,27 +1,24 @@
 let data = {
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: 
-    {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
-    },
-    phone: "1-770-736-8031 x56442",
-    website: "hildegard.org",
-    }
-    //Ubahlah data  tersebut menggunakan spread operator 
-    let newData = {...data, hobby:"futsal"}
-    newData.name = "calvin rahmat"
-    newData.email = "calvin.rahmat@gmail.com"
+	id: 1,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'Sincere@april.biz',
+	address: {
+		street: 'Kulas Light',
+		suite: 'Apt. 556',
+		city: 'Gwenborough',
+		zipcode: '92998-3874',
+	},
+	phone: '1-770-736-8031 x56442',
+	website: 'hildegard.org',
+};
+//Ubahlah data  tersebut menggunakan spread operator
+let newData = { ...data, hobby: 'futsal' };
+newData.name = 'calvin rahmat';
+newData.email = 'calvin.rahmat@gmail.com';
 
+console.log(newData);
 
-    console.log(newData)
-
-    //Ambilah data “street dan city” tersebut menggunakan destructuring
-    let {street,city}=data.address
-    console.log(street + ' '+ city)
-
+//Ambilah data “street dan city” tersebut menggunakan destructuring
+let { street, city } = data.address;
+console.log(street + ' ' + city);
