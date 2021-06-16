2b3e36cabe49c5bc140c5bfef3e7079a50b0a6e1
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-16 20:48:11 +0700

    Merapihkan line

--- tugas1.js
+++ tugas1.js
@@ -1,48 +1,45 @@
 let biodata = {
-    name : "Calvin Rahmat",
-    age: 18,
-    hobbies: ['gaming', 'reading', 'futsal'],
-    isMarried: false,
-    schoolList : [
-        {
-            name: 'SDN Pengadilan 3 Bogor',
-            yearIn: 2002,
-            yearOut: 2008,
-            major: null
-        },
-        {
-            name: ' SMPN 12 BOGOR',
-            yearIn: 2008,
-            yearOut: 2011,
-            major: null
-        },
-        {
-            name: 'SMAN 3 BOGOR',
-            yearIn: 2011,
-            yearOut: 2014,
-            major: 'IPA'
-        },
-        {
-            name: 'Binus University',
-            yearIn: 2014,
-            yearOut:2018,
-            major: 'computer engineering'
-        }
-    ],
-    skills: [
-        {
-            skillName: 'Microsoft',
-            level: 'advaced',
-        },
-        {
-            skillName: 'Product Knowledge',
-            level: 'beginner'
-        }
-    ],
-    InterestInCodein :true,
-       
-}
+	name: 'Calvin Rahmat',
+	age: 18,
+	hobbies: ['gaming', 'reading', 'futsal'],
+	isMarried: false,
+	schoolList: [
+		{
+			name: 'SDN Pengadilan 3 Bogor',
+			yearIn: 2002,
+			yearOut: 2008,
+			major: null,
+		},
+		{
+			name: ' SMPN 12 BOGOR',
+			yearIn: 2008,
+			yearOut: 2011,
+			major: null,
+		},
+		{
+			name: 'SMAN 3 BOGOR',
+			yearIn: 2011,
+			yearOut: 2014,
+			major: 'IPA',
+		},
+		{
+			name: 'Binus University',
+			yearIn: 2014,
+			yearOut: 2018,
+			major: 'computer engineering',
+		},
+	],
+	skills: [
+		{
+			skillName: 'Microsoft',
+			level: 'advaced',
+		},
+		{
+			skillName: 'Product Knowledge',
+			level: 'beginner',
+		},
+	],
+	InterestInCodein: true,
+};
 
-console.log(biodata)
-    
-   
+console.log(biodata);
